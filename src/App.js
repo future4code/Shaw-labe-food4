88892e645994982d4a7d2b9bc8ca6d962cf0c827
servuloccsilva <servuloccsilva@gmail.com>
2022-05-16 13:07:23 -0300

    Início do Login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {LoginPage} from "./pages/LoginPage/LoginPage"
 
 
 function App() {
   return (
     <div className="App">
     <h1>LabeFood</h1>
+    <LoginPage/>
     </div>
   );
 }
---
+++ src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
+
+export function LoginPage() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="E-mail" variant="outlined" />
+        <TextField id="outlined-basic" label="Senha" variant="outlined" />
+      </form>
+      <div className={classes.root}>
+      <Button variant="contained" color="secondary">
+        Entrar
+      </Button>
+      <p>Não possui cadastro? Clique aqui.</p>
+      
+    </div>
+    </div>
+
+  );
+}
