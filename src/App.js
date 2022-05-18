3fa92027b1f8d8fd31e5395b29e6f3ea2e97d31a
Rodrigo Santos <rodrigo.v.santos89@gmail.com>
2022-05-18 13:18:29 -0300

    Criei o thema do MUI e já setei as cores do projeto como primária e secundária

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import Router from "./router/Router";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core";
+import theme from "./components/Theme/theme";
+
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <BrowserRouter>
         <Router />
     </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
